29ef1d40b3d99a6ac318cd9440ef537f26321845
Daniel Rosenqvist <daniel.wibergrosenqvist@gmail.com>
2019-12-26 17:32:20 +0100

    Added exception for leap years, refactored CSS and HTML

--- js/script.js
+++ js/script.js
@@ -1,25 +1,33 @@
 // Setup the neccessary variables and calculate which week it is.
 function calculateWeek () {
-    const date = new Date(),
-          month = date.getMonth(),
-          day = date.getDate(),
-          daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+  const date = new Date(),
+        year = 2020,
+        month = date.getMonth(),
+        day = date.getDate(),
+        daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
-    let output = day
-
-    for (let i = 0; i < month; i++) {
-        output += daysInMonth[i]
-    }
+  // Changes the #days in February if it is a leap year
+  if (year % 4 === 0) {
+    daysInMonth[1] = 29;
+  }
     
-    return(Math.floor(output / 7 + 1))
-}
+  // Intializing the output with the #day of the current month
+  let output = day;
 
+  // Loops through and adds the #days of the months that have passed
+  for (let i = 0; i < month; i++) {
+      output += daysInMonth[i];
+  }
+    
+  // Divides the #days to get the #week
+  return(Math.floor(output / 7 + 1));
+}
 
 // Outputs the week number to the HTML h1 element
 function outputWeek () {
-    const outputField = document.querySelector('#weekOutput')
+  const outputField = document.querySelector('.weekOutput');
 
-    outputField.textContent = calculateWeek()
+  outputField.textContent = calculateWeek();
 }
 
-outputWeek()
+outputWeek();
